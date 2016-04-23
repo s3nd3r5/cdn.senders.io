d06a267a389c4fc2003df376af05f33724e8adab
Stephen Enders <PUT YOUR MFA ENABLED  PASS HERE>
2016-04-23 10:45:19 -0400

    WIP CDN Page

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var jade = require('gulp-jade');
 var del = require('del');
+var path = require('path');
 
 gulp.task('clean',function(){
 	return del(['bin','*.log']);
@@ -15,8 +16,15 @@ gulp.task('jade',['clean'],function(){
 
 gulp.task('less',['clean'],function(){
 	gulp.src('src/less/index.less')
-	.pipe(less())
+	.pipe(less({
+	   paths: [ path.join(__dirname, 'less', 'includes') ]	
+	}))
 	.pipe(gulp.dest('bin/css/'));
 });
 
 gulp.task('build',['clean','jade','less']);
+
+gulp.task('watch',function(){
+	gulp.watch('src/**/*',['build']);
+});
+
